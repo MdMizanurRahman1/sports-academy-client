0ba62d60c44efaf8fba24793d8fcae705557fddf
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-06-13 23:20:14 +0300

    manage users done

--- src/Dashboard/AllUsers/ManageUsers.jsx
+++ src/Dashboard/AllUsers/ManageUsers.jsx
@@ -1,64 +1,92 @@
-import React from 'react';
+
+
+import React, { useState, useEffect } from 'react';
 import useClass from '../../hooks/useClass';
 
-
 const ManageUsers = () => {
-
+    const [approvalState, setApprovalState] = useState();
     const [myclass] = useClass();
     console.log(myclass, 'this is my class manage');
 
+    useEffect(() => {
+        localStorage.setItem('myclass', JSON.stringify(myclass));
+    }, [myclass]);
+
+    const handleApprove = (index) => {
+        const updatedClass = [...myclass];
+        updatedClass[index].status = 'approve';
+        setApprovalState(updatedClass);
+    };
+
+    const handleDeny = (index) => {
+        const updatedClass = [...myclass];
+        updatedClass[index].status = 'deny';
+        setApprovalState(updatedClass);
+    };
 
     return (
         <div className="overflow-x-auto py-16 mx-10 w-full">
-            <h2 className='text-center font-semibold text-2xl my-7'>All Instructors</h2>
-            <table className="table ">
-
-
+            <h2 className="text-center font-semibold text-2xl my-7">All Instructors</h2>
+            <table className="table">
                 <thead>
                     <tr>
                         <th>#</th>
                         <th>Image</th>
                         <th>Name</th>
-                        <th>Instructor name</th>
-                        <th>Email</th>
+                        <th>Instructor Name/Email</th>
                         <th>Status</th>
                         <th>Enrolled</th>
-                        <th>Feedback</th>
-                        <th>Update</th>
+                        <th>Available Seats</th>
+                        <th>Price</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-
-                    {
-                        myclass.map((instructor, index) =>
-                            <tr key={instructor._id}>
-                                <td>{index + 1}</td>
-                                <td>
-                                    <div className="flex items-center space-x-3">
-                                        <div className="avatar">
-                                            <div className="mask mask-squircle w-12 h-12">
-                                                <img src={instructor.image} alt="Avatar Tailwind CSS Component" />
-                                            </div>
+                    {myclass.map((instructor, index) => (
+                        <tr key={instructor._id}>
+                            <td>{index + 1}</td>
+                            <td>
+                                <div className="flex items-center space-x-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={instructor.image} alt="Avatar Tailwind CSS Component" />
                                         </div>
                                     </div>
-                                </td>
-                                <td className='text-md font-semibold'>
-                                    {instructor.name}
-                                </td>
-                                <td>{instructor.instructor_name}</td>
-                                <td>{instructor.email}</td>
-                                <td>{instructor.status}</td>
-                                <td>{instructor.enrolled}</td>
-                                <th>
-                                    <button className="btn btn-error btn-outline btn-xs">Feedback</button>
-                                </th>
-                                <th>
-                                    <button className="btn btn-success btn-outline btn-xs">update</button>
-                                </th>
-                            </tr>
-
-                        )
-                    }
+                                </div>
+                            </td>
+                            <td>{instructor.name}</td>
+                            <td>
+                                {instructor.instructor_name}
+                                <br />
+                                <span className="badge badge-ghost badge-sm">{instructor.email}</span>
+                            </td>
+                            <td>{instructor.status}</td>
+                            <td>{instructor.enrolled}</td>
+                            <td>{instructor.seats}</td>
+                            <td>{instructor.price}</td>
+                            <td>
+                                <button
+                                    className='bg-green-200 px-2 rounded btn btn-xs'
+                                    disabled={instructor.status === 'approve' || instructor.status === 'deny'}
+                                    onClick={() => handleApprove(index)}
+                                >
+                                    Approve
+                                </button>
+                                <br />
+                                <button
+                                    className='bg-red-200 px-2 rounded btn btn-xs'
+                                    disabled={instructor.status === 'approve' || instructor.status === 'deny'}
+                                    onClick={() => handleDeny(index)}
+                                >
+                                    Deny
+                                </button>
+                                <br />
+                                <button className='bg-orange-200 px-2 rounded btn btn-xs '>
+                                    Feedback
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
